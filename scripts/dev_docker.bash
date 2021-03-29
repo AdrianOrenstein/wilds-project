4b2c15981b90cb7d73a541f88e857a1835ecf637
Adrian <adrian.orenstein@gmail.com>
2021-03-29 15:34:13 +1030

    rolled cuda11 docker

--- scripts/dev_docker.bash
+++ scripts/dev_docker.bash
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-cmp_volumes="--volume=$(pwd):/home/wilds-project/:rw"
+cmp_volumes="--volume=$(pwd):/app/:rw"
 
 docker run --rm -ti \
     $cmp_volumes \
     -it \
     -p 8888:8888 \
     --gpus all \
+    --ipc host \
     --user "$(id -u):$(id -g)" \
     wilds-project \
     bash 
